feb6e14569727ceba0e4413c9bf9312101cca226
savan <kansagrasavan108@gmail.com>
2024-03-13 11:02:41 +0000

    added the visulization

--- visualize_log.py
+++ visualize_log.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 log_data = pd.read_csv('logs/lstm_layer_2_unit_64.csv')
 
 plt.figure(figsize=(8, 5))
-plt.plot(log_data['iteration'], log_data['test_RMSE'], marker='o', linestyle='-', color='b')
+plt.plot(log_data['Iteration'], log_data['test_RMSE'], marker='o', linestyle='-', color='b')
 plt.title('test_RMSE Over iteration')
 plt.xlabel('iteratuib')
 plt.ylabel('test_RMSE')
